326197f0f580ba12f8727ee7992868dca41f3465
Abdüssamed Erkalp <122685548+Abdussamed-1@users.noreply.github.com>
2025-12-25 23:22:53 +0300

    Work on through variables and comment line for second day

---
+++ day2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Hello World!");
+Console.WriteLine("Hello World!");
+Console.WriteLine("I am Learning C#");
+Console.WriteLine("It is awesome!");
+Console.WriteLine(3 + 3);
+Console.Write("Hello World! ");
+Console.Write("I will print on the same line.");
+// This is a comment
+Console.WriteLine("Hello World!")
+Console.WriteLine("Hello World!"); // This is another comment
+/* The code below will print the words Hello World
+to the screen, and it is amazing */
+Console.WriteLine("Hello World!");
+// Variables
+// type variableName = value;
+string name = "John";
+Console.WriteLine(name);
+int myNum = 15;
+Console.WriteLine(myNum);
