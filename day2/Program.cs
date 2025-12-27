049a4c843d2262acd012a5345ab65ee4cc725c0b
Abdüssamed Erkalp <122685548+Abdussamed-1@users.noreply.github.com>
2025-12-27 04:35:24 +0300

    Update all folder and file

--- day2/Program.cs
+++ day2/Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine(3 + 3);
 Console.Write("Hello World! ");
 Console.Write("I will print on the same line.");
 // This is a comment
-Console.WriteLine("Hello World!")
+Console.WriteLine("Hello World!"); // This is a comment
 Console.WriteLine("Hello World!"); // This is another comment
 /* The code below will print the words Hello World
 to the screen, and it is amazing */
---
+++ day3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace day3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //int myNum = 100000;
+        long myNum = 15000000000L;
+        Console.WriteLine(myNum);
+        Console.ReadKey();
+    }
+}
