700fead23fc03b7214d97ca7ba4a8582f86ab673
Abdüssamed Erkalp <122685548+Abdussamed-1@users.noreply.github.com>
2026-01-04 23:36:45 +0300

    modified on throug Program.cs if else condition and while-do loop

--- day6/Program.cs
+++ day6/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+while (condition) 
+{
+  // code block to be executed
+}
+*/
+using System;
+namespace day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //whileloop();
+            /*do
+            {
+                // code block to be executed
+            }
+            while (condition);
+            */
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 6);
+        }
+
+        private static void whileloop()
+        {
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
