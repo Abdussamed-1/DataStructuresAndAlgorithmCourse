d294f6a59713a5a4f7708c6b1d6cf8c211858a63
Abdüssamed Erkalp <122685548+Abdussamed-1@users.noreply.github.com>
2026-01-05 23:09:28 +0300

    Update on project it is adding for loop

---
+++ day7/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * for (statement 1; statement 2; statement 3) 
+{
+  // code block to be executed
+}
+*/
+using System;
+
+namespace day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i = i + 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
