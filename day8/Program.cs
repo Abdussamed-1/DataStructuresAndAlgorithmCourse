cbf62b7118f06c634e5ff6a27f146e0ac93974ac
Abdüssamed Erkalp <122685548+Abdussamed-1@users.noreply.github.com>
2026-01-07 23:49:09 +0300

    array lists and control list in elements

--- day8/Program.cs
+++ day8/Program.cs
@@ -1,1 +1,23 @@
 ﻿
+using System;
+
+namespace day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string[] cars;
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            Console.WriteLine(cars[0]);
+            // Outputs Volvo
+            int[] myNum = { 10, 20, 30, 40 };
+            cars[0] = "Opel";
+            Console.WriteLine(cars.Length);
+            // Outputs 4
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
