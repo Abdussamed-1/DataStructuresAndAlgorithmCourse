5a64014ce390f6ca11a6ec2cbdf3777362ccf460
Abdüssamed Erkalp <122685548+Abdussamed-1@users.noreply.github.com>
2026-01-12 16:51:20 +0300

    update new problem solutions from leetcode

--- day9/Program.cs
+++ day9/Program.cs
@@ -4,6 +4,28 @@ namespace day9;
 
 public class Solution
 {
+        public string Convert(string s, int numRows)
+        {
+            int len = s.Length;
+            if (numRows == 1 || numRows >= s.Length)
+                return s;
+            List<string> rows = new List<string>();
+            for (int i = 0; i < Math.Min(numRows, s.Length); i++)
+                rows.Add("");
+            int curRow = 0;
+            bool goingDown = false;
+            foreach (char c in s)
+            {
+                rows[curRow] += c;
+                if (curRow == 0 || curRow == numRows - 1)
+                    goingDown = !goingDown;
+                curRow += goingDown ? 1 : -1;
+            }
+            string ret = "";
+            foreach (string row in rows)
+                ret += row;
+            return ret;
+        }
     public string LongestPalindrome(string s)
     {
         int start = 0, end = 0;
