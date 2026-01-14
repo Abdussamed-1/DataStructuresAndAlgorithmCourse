43d00bc9d095a0aa778e73fa8ed53a9dd2fd5b01
Abdüssamed Erkalp <122685548+Abdussamed-1@users.noreply.github.com>
2026-01-14 19:54:13 +0300

    update new problem solution from codewars

--- day9/Program.cs
+++ day9/Program.cs
@@ -2,6 +2,28 @@
 using System;
 namespace day9;
 
+public static class Kata
+{
+    public static string Likes(string[] name)
+    {
+
+        throw new NotImplementedException();
+        if (name == null) ;
+        if (name.Length == 0)
+            return "no one likes this";
+        else if (name.Length == 1)
+            return $"{name[0]} likes this";
+        else if (name.Length == 2)
+            return $"{name[0]} and {name[1]} like this";
+        else if (name.Length == 3)
+            return $"{name[0]}, {name[1]} and {name[2]} like this";
+        else
+            return $"{name[0]}, {name[1]} and {name.Length - 2} others like this";
+
+    }
+}
+
+
 public class Solution
 {
     public bool IsPalindrome(int x)
