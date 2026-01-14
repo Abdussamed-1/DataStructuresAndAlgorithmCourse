7d68dff76bf7682d9a9e813dbbbf64843d2eb5c2
Abdüssamed Erkalp <122685548+Abdussamed-1@users.noreply.github.com>
2026-01-14 23:47:09 +0300

    update new problem solutions from leetcode for integer to roman

--- day9/Program.cs
+++ day9/Program.cs
@@ -29,6 +29,27 @@ public static class Kata
 public class Solution
 {
     public:
+    string intToRoman(int num)
+    {
+        vector<pair<int, string>> val = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"},
+            {1, "I"}
+        };
+        string res = "";
+        for (const auto& [v, s] : val)
+        {
+            while (num >= v)
+            {
+                num -= v;
+                res += s;
+            }
+        }
+        return res;
+
+    }
+    public:
     int maxArea(vector<int>& height)
     {
         int left = 0;
