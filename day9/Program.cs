bd9b362ae31094de67cb432dd5110038b6f45a75
Abdüssamed Erkalp <122685548+Abdussamed-1@users.noreply.github.com>
2026-01-15 20:53:52 +0300

    Modified on through project upload new problem solution from leetcode

--- day9/Program.cs
+++ day9/Program.cs
@@ -23,6 +23,20 @@ public static class Kata
 
 public class Solution
 {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length == 0) return "";
+            string prefix = strs[0];
+            for (int i = 1; i < strs.Length; i++)
+            {
+                while (strs[i].IndexOf(prefix) != 0)
+                {
+                    prefix = prefix.Substring(0, prefix.Length - 1);
+                    if (string.IsNullOrEmpty(prefix)) return "";
+                }
+            }
+            return prefix;
+        }
     public int RomanToInt(string s)
     {
         if (string.IsNullOrEmpty(s)) return 0;
