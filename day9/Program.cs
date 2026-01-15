9d40f31fe460af176dd09dfdb292a8986a6fa51a
Abdüssamed Erkalp <122685548+Abdussamed-1@users.noreply.github.com>
2026-01-15 20:37:48 +0300

    Update problem solutions from leetcode for string to int

--- day9/Program.cs
+++ day9/Program.cs
@@ -23,6 +23,33 @@ public static class Kata
 
 public class Solution
 {
+    public int RomanToInt(string s)
+    {
+        if (string.IsNullOrEmpty(s)) return 0;
+        var romanMap = new Dictionary<char, int>
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        int total = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (i + 1 < s.Length && romanMap[s[i]] < romanMap[s[i + 1]])
+            {
+                total -= romanMap[s[i]];
+            }
+            else
+            {
+                total += romanMap[s[i]];
+            }
+        }
+        return total;
+    }
     // C++ -> C# conversion: intToRoman
     public string IntToRoman(int num)
     {
