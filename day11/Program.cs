06e82f87b61db308c806f2357e74cb9037e60f2b
Abdüssamed Erkalp <122685548+Abdussamed-1@users.noreply.github.com>
2026-01-16 22:51:22 +0300

    it adds to create the method on c#

--- day11/Program.cs
+++ day11/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Create a method inside the Program class:
+
+using System;
+
+class Program
+{
+    static void MyMethod()
+    {
+        /*
+         * Example Explained
+         MyMethod() is the name of the method
+         static means that the method belongs to the Program class and not an object of the Program class. You will learn more about objects and how to access methods through objects later in this tutorial.
+         void means that this method does not have a return value. You will learn more about return values later in this chapter
+          */
+    }
+}
