fab999f69bdd36f807f00ceacb0bcccb70a9b1ae
Abdüssamed Erkalp <122685548+Abdussamed-1@users.noreply.github.com>
2026-01-16 23:57:42 +0300

    update day9

--- day9/Program.cs
+++ day9/Program.cs
@@ -23,6 +23,38 @@ public static class Kata
 
 public class Solution
 {
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        int n = nums.Length;    
+        Array.Sort(nums);
+        var result = new List<IList<int>>();
+        for (int i = 0; i < n - 2; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right)
+            {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0)
+                {
+                    result.Add(new List<int> { nums[i], nums[left], nums[right] });
+                    while (left < right && nums[left] == nums[left + 1]) left++; // Skip duplicates
+                    while (left < right && nums[right] == nums[right - 1]) right--; // Skip duplicates
+                    left++;
+                    right--;
+                }
+                else if (sum < 0)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+        }
+    }
     public List<int> SubarraySum(int[] arr, int target)
     {
         var result = new List<int>();
