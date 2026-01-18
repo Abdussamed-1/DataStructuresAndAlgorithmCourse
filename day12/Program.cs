24d228478165f568058d7fd922f4b14ad3582430
Abdüssamed Erkalp <122685548+Abdussamed-1@users.noreply.github.com>
2026-01-18 22:53:51 +0300

    İt creating for new project for Object Oriented programming language in that case we will determine class and relational variables

---
+++ day12/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+What is OOP?
+OOP stands for Object-Oriented Programming.
+Procedural programming is about writing procedures or methods that perform operationson the data, 
+while object-oriented programming is about creating objects that contain both data and methods.
+ */
+
+using System;
+
+namespace MyApplication
+{
+    class Car
+    {
+        string color = "red";
+
+        static void Main(string[] args)
+        {
+            Car myObj1 = new Car();
+            Car myObj2 = new Car();
+            Console.WriteLine(myObj1.color);
+            Console.WriteLine(myObj2.color);
+        }
+    }
+}
