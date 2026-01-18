5c3f0bd000787723b7ba4d51e83b2c7926bc362b
Abdüssamed Erkalp <122685548+Abdussamed-1@users.noreply.github.com>
2026-01-18 23:28:45 +0300

    Added color and speed fields and an accelerate method to the Car class in the Day12 project.

--- day12/Program.cs
+++ day12/Program.cs
@@ -1,24 +1,33 @@
 ﻿/*
 What is OOP?
 OOP stands for Object-Oriented Programming.
-Procedural programming is about writing procedures or methods that perform operationson the data, 
+Procedural programming is about writing procedures or methods that perform operations on the data,
 while object-oriented programming is about creating objects that contain both data and methods.
- */
+*/
 
 using System;
 
-namespace MyApplication
+namespace day12
 {
     class Car
     {
-        string color = "red";
+        public string Color = "red";
+        public int MaxSpeed = 200;
+    }
 
+    public class Program
+    {
         static void Main(string[] args)
         {
             Car myObj1 = new Car();
             Car myObj2 = new Car();
-            Console.WriteLine(myObj1.color);
-            Console.WriteLine(myObj2.color);
+
+            Console.WriteLine(myObj1.Color);
+            Console.WriteLine(myObj2.Color);
+
+            Car myObj = new Car();
+            Console.WriteLine(myObj.Color);
+            Console.WriteLine(myObj.MaxSpeed);
         }
     }
 }
