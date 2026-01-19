e32de87c3ef95b6acd331bc89ab5a093aa97c46e
Abdüssamed Erkalp <122685548+Abdussamed-1@users.noreply.github.com>
2026-01-19 23:20:28 +0300

    modified on through projcet fixed code and re-edited

--- day10/Program.cs
+++ day10/Program.cs
@@ -17,7 +17,6 @@ class Result
 
     /*
      * Complete the 'reverseArray' function below.
-     *
      * The function is expected to return an INTEGER_ARRAY.
      * The function accepts INTEGER_ARRAY a as parameter.
      */
