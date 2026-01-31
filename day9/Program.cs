680b34f6c7aa538e2de80acea4f2461c0c88ccca
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-01-31 21:29:46 +0300

    Problem solutions from leetcode

--- day9/Program.cs
+++ day9/Program.cs
@@ -39,6 +39,39 @@ public static class Kata
 
 public class Solution
 {
+
+    public IList<string> LetterCombinations(string digits)
+    {
+        if (string.IsNullOrEmpty(digits)) return new List<string>();
+        var phoneMap = new Dictionary<char, string>
+        {
+            {'2', "abc"},
+            {'3', "def"},
+            {'4', "ghi"},
+            {'5', "jkl"},
+            {'6', "mno"},
+            {'7', "pqrs"},
+            {'8', "tuv"},
+            {'9', "wxyz"}
+        };
+        var result = new List<string>();
+        void Backtrack(int index, string path)
+        {
+            if (index == digits.Length)
+            {
+                result.Add(path);
+                return;
+            }
+            string possibleLetters = phoneMap[digits[index]];
+            foreach (char letter in possibleLetters)
+            {
+                Backtrack(index + 1, path + letter);
+            }
+        }
+        return result;
+    }
+
+
     public int ThreeSumClosest(int[] nums, int target)
     { 
         Array.Sort(nums);
