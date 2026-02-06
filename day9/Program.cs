35fdb5df70c8da1e8699e325be295de4e38eb29b
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-02-06 23:33:22 +0300

    From leetcode it solves problem and Remove Nth From End

--- day9/Program.cs
+++ day9/Program.cs
@@ -39,7 +39,22 @@ public static class Kata
 
 public class Solution
 {
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        var dummy = new ListNode(0) { next = head };
+        var first = dummy;
+        var second = dummy;
+        for (int i = 0; i <= n; i++)
+            first = first.next;
+        while (first != null)
+        {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
 
+    }
     public IList<IList<int>> FourSum(int[] nums, int target)
     {
         Array.Sort(nums);
