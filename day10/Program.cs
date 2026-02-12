dfba57a260fa081a1b1ce1d1ddaebe4b34550f97
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-02-12 22:16:56 +0300

    Modified on through it adding new examples from course

--- day10/Program.cs
+++ day10/Program.cs
@@ -11,47 +11,32 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
-
-internal class loop
-{
-    static void Main(string[] args)
-    {
-        int i = 0;
-        while (i < 10)
-        {
-            Console.WriteLine("{0,-3} {1,-3}", i);
-            i++;
-        }
-        Console.WriteLine("Döngü Sonu!");
-        Console.ReadKey();
-    }
-
-}
-
 /*
 class Result
 {
-    * Complete the 'reverseArray' function below.
-    * The function is expected to return an INTEGER_ARRAY.
-    * The function accepts INTEGER_ARRAY a as parameter.
-
-   public static List<int> reverseArray(List<int> a)
-   {
-       a.Reverse();
-       return a;
-   }
+     * Complete the 'reverseArray' function below.
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     
+    public static List<int> reverseArray(List<int> a)
+    {
+        a.Reverse();
+        return a;
+    }
 }*/
-
-class Program : loop
+namespace day 10
+{
+public class Program
 {
     static void Main(string[] args)
     {
         int sayac = 1;
-        while (sayac < 10)
+        while (sayac <= 10)
         {
-            Console.WriteLine("{0,-3} {1,-3}", sayac, sayac*sayac);
-            sayac++;
+            Console.WriteLine("{0,-3} {1,-3}", sayac, sayac * sayac);
+            sayac += 1;
         }
         Console.ReadKey();
     }
+}
 }
