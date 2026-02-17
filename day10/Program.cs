ca569655ca63a443832ef51f0981afb938db5d59
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-02-17 23:39:24 +0300

    Modified on last problem solutions it added to private static void called generateparanthesishelper for helpers problem solving

--- day10/Program.cs
+++ day10/Program.cs
@@ -30,9 +30,23 @@ namespace day10
     {
         public IList<string> GenerateParenthesis(int n)
         {
-            var result = new List<string>();
-            GenerateParenthesis(result, string.Empty, n, n);
+            IList<string> result = new List<string>();
+            GenerateParenthesisHelper(result, "", 0, 0, n);
             return result;
+        }
+        private void GenerateParenthesisHelper(IList<string> result, string current, int open, int close, int n)
+        {
+            if (current.Length == 2 * n)
+            {
+                result.Add(current);
+                return;
+            }
+
+            if (open < n)
+                GenerateParenthesisHelper(result, current + "(", open + 1, close, n);
+
+            if (close < open)
+                GenerateParenthesisHelper(result, current + ")", open, close + 1, n);
         }
         public ListNode MergeTwoLists(ListNode list1, ListNode list2)
         {
