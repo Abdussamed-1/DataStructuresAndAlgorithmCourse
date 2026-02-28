2d9d1836eaa76546283ec84646cc8c2e636c386d
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-02-28 23:08:18 +0300

    Add StrStr method for substring search in Solution class
    Added StrStr to Solution class in day10 namespace. This method returns the index of the first occurrence of needle in haystack, or -1 if not found. Returns 0 if needle is empty or null. Implements basic substring search functionality.

--- day10/Program.cs
+++ day10/Program.cs
@@ -38,6 +38,17 @@ namespace day10
     }
     public class Solution
     {
+        public int StrStr(string haystack, string needle)
+        {
+            if (string.IsNullOrEmpty(needle))
+                return 0;
+            for (int i = 0; i <= haystack.Length - needle.Length; i++)
+            {
+                if (haystack.Substring(i, needle.Length) == needle)
+                    return i;
+            }
+            return -1; 
+        }
         public int RemoveElement(int[] nums, int val)
         {
             int newLength = 0;
