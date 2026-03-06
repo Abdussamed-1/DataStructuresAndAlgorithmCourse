632fb91e5091a3f57b1d68e5524484fa8d6beb0f
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-03-06 19:55:01 +0300

    Add NextPermutation method to Solution class

--- day10/Program.cs
+++ day10/Program.cs
@@ -38,6 +38,42 @@ namespace day10
     }
     public class Solution
     {
+        public void NextPermutation(int[] nums)
+        {
+            if (nums == null || nums.Length <= 1) return;
+
+            int i = nums.Length - 2;
+            while (i >= 0 && nums[i] >= nums[i + 1])
+                i--;
+
+            if (i >= 0)
+            {
+                int j = nums.Length - 1;
+                while (nums[j] <= nums[i])
+                    j--;
+                Swap(nums, i, j);
+            }
+
+            Reverse(nums, i + 1);
+        }
+
+        private void Swap(int[] nums, int i, int j)
+        {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+
+        private void Reverse(int[] nums, int start)
+        {
+            int i = start, j = nums.Length - 1;
+            while (i < j)
+            {
+                Swap(nums, i, j);
+                i++;
+                j--;
+            }
+        }
         public IList<int> FindSubstring(string s, string[] words)
         {
             IList<int> result = new List<int>();
