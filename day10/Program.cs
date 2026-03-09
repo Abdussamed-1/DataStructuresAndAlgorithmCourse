0849a09573c9ba4b5945477a1ad76ff778981ce2
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-03-09 23:29:09 +0300

    it added from leetcode problem solutions and search algorithm

--- day10/Program.cs
+++ day10/Program.cs
@@ -38,6 +38,31 @@ namespace day10
     }
     public class Solution
     {
+        public int Search(int[] nums, int target)
+        {
+            int left = 0, right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target)
+                    return mid;
+                if (nums[left] <= nums[mid]) // Left half is sorted
+                {
+                    if (nums[left] <= target && target < nums[mid])
+                        right = mid - 1; // Target is in the left half
+                    else
+                        left = mid + 1; // Target is in the right half
+                }
+                else // Right half is sorted
+                {
+                    if (nums[mid] < target && target <= nums[right])
+                        left = mid + 1; // Target is in the right half
+                    else
+                        right = mid - 1; // Target is in the left half
+                }
+            }
+            return -1; // Target not found
+        }
         public int LongestValidParentheses(string s)
         {
             int maxLength = 0;
