d8682d8daa846019720778f06f9946fd374e73ce
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-03-14 23:57:19 +0300

    Add SearchInsert method with binary search logic

--- day10/Program.cs
+++ day10/Program.cs
@@ -38,6 +38,23 @@ namespace day10
     }
     public class Solution
     {
+
+        public int SearchInsert(int[] nums, int target)
+        {
+            int left = 0, right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target)
+                    return mid;
+                else if (nums[mid] < target)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            return left; // Left is the insertion point
+        }
+
         public int[] SearchRange(int[] nums, int target)
         {
             int[] result = new int[2] { -1, -1 };
