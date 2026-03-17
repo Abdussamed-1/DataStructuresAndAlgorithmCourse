b6e8b082a04571bc3dd2c0b6d9f2e703b829cf72
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-03-17 23:16:12 +0300

    Add CountAndSay method to Solution class

--- day10/Program.cs
+++ day10/Program.cs
@@ -38,6 +38,26 @@ namespace day10
     }
     public class Solution
     {
+        public string CountAndSay(int n)
+        {   if (n == 1) return "1";
+            string prev = CountAndSay(n - 1);
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+            for (int i = 1; i < prev.Length; i++)
+            {
+                if (prev[i] == prev[i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    sb.Append(count).Append(prev[i - 1]);
+                    count = 1;
+                }
+            }
+            sb.Append(count).Append(prev[prev.Length - 1]);
+            return sb.ToString();
+        }
         public void SolveSudoku(char[][] board)
         {
             Solve(board);
