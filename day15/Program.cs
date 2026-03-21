509447b84e73990676c3a483ff69252546906eee
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-03-21 18:40:55 +0300

    Update folder name changed

---
+++ day15/Program.cs
@@ -0,0 +1,13 @@
+﻿class program
+{
+    static void Main(string[], args)
+    {
+        int sayac = 0;
+        while (sayac <= 10)
+        {
+            Console.WriteLine("{0,-3} {1,-3}", sayac, sayac * sayac);
+            sayac += 1;
+        }
+        Console.ReadKey();
+    }
+}
