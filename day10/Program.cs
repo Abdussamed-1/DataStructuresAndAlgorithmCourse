d8ea7c4c31146d89c9e6101185e21a6fae7d87e7
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-03-24 23:06:21 +0300

    Add string multiplication method to Solution class

--- day10/Program.cs
+++ day10/Program.cs
@@ -38,6 +38,36 @@ namespace day10
     }
     public class Solution
     {
+        public bool IsMatch(string s, string p)
+        {
+            int m = s.Length, n = p.Length;
+            bool[,] dp = new bool[m + 1, n + 1];
+            dp[0, 0] = true;
+            for (int j = 1; j <= n; j++)
+            {
+                if (p[j - 1] == '*')
+                    dp[0, j] = dp[0, j - 2];
+            }
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (p[j - 1] == '.' || p[j - 1] == s[i - 1])
+                    {
+                        dp[i, j] = dp[i - 1, j - 1];
+                    }
+                    else if (p[j - 1] == '*')
+                    {
+                        dp[i, j] = dp[i, j - 2]; // Zero occurrence
+                        if (p[j - 2] == '.' || p[j - 2] == s[i - 1])
+                        {
+                            dp[i, j] |= dp[i - 1, j]; // One or more occurrences
+                        }
+                    }
+                }
+            }
+            return dp[m, n];
+        }
         public string Multiply(string num1, string num2)
         {
             if (num1 == "0" || num2 == "0") return "0";
