a0587f286b8d5f3e4d3e9f04eac419c3adf18d7f
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-03-27 01:44:18 +0300

    İt added to from ten to zero and their square expressions

--- day15/Program.cs
+++ day15/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 
-public class Program
+class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
+        // 0'dan 10'a kadar
         int sayac = 0;
         while (sayac <= 10)
         {
             Console.WriteLine("{0,-3} {1,-3}", sayac, sayac * sayac);
-            sayac += 1;
+            sayac++;
         }
+
+        Console.WriteLine("----------");
+
+        // 10'dan 0'a kadar
+        sayac = 10;
+        while (sayac >= 0)
+        {
+            Console.WriteLine("{0,-3} {1,-3}", sayac, sayac * sayac);
+            sayac--;
+        }
+
         Console.ReadKey();
     }
 }
