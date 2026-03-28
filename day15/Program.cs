86953ed38cb065bd78e198dfa45e030b609b5d90
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-03-28 15:54:28 +0300

    İt added to do-while loops from Data structure and algorithm course

--- day15/Program.cs
+++ day15/Program.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     public static void Main(string[] args)
     {
+       for (int i = 0; i < 50; i+=1)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("Can you input number:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+        int i = 2;
+        do
+        {
+            Console.WriteLine(i);
+            i += 2; // i = i + 2;
+        } while (i <= n);
+
         // 0'dan 10'a kadar
         int sayac = 0;
         while (sayac <= 10)
