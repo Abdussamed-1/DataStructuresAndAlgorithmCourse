bde11e56e231e36c057cf58863617f62d1f1bc1b
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-04-01 23:13:55 +0300

    İt added to problem solutions from leetcode

--- day10/Program.cs
+++ day10/Program.cs
@@ -38,7 +38,21 @@ namespace day10
     }
     public class Solution
     {
-
+        public void Rotate(int[][] matrix)
+        {
+            int n = matrix.Length;
+            for (int i = 0; i < n / 2; i++)
+            {
+                for (int j = i; j < n - 1 - i; j++)
+                {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[n - 1 - j][i];
+                    matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                    matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                    matrix[j][n - 1 - i] = temp;
+                }
+            }
+        }
         public IList<IList<int>> PermuteUnique(int[] nums)
         {
             IList<IList<int>> result = new List<IList<int>>();
