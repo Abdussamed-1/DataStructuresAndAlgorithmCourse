40fbe4728472a37c87a5d3520e8a1d9b6dc95ed8
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-04-10 21:59:20 +0300

    Add CanJump method to Solution
    Implement CanJump in Solution (day10/Program.cs) using a greedy max-reach algorithm for the Jump Game problem. Iterates through nums, returns false if current index exceeds the furthest reachable index, updates maxReach with i + nums[i], and returns true if the end is reachable.

--- day10/Program.cs
+++ day10/Program.cs
@@ -38,6 +38,17 @@ namespace day10
     }
     public class Solution
     {
+        public bool CanJump(int[] nums)
+        {
+            int maxReach = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i > maxReach)
+                    return false;
+                maxReach = Math.Max(maxReach, i + nums[i]);
+            }
+            return true;
+        }
         public IList<int> SpiralOrder(int[][] matrix)
         {
             IList<int> result = new List<int>();
