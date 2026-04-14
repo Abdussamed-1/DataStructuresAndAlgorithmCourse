87e418ddc885c5123a4bccd6c23de75337eaf558
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-04-14 22:54:29 +0300

    Add GenerateMatrix to produce spiral n x n matrix
    Introduce Solution.GenerateMatrix(int n) which returns an n x n int[][] filled with consecutive integers in clockwise spiral order. The implementation uses top/bottom/left/right boundaries and layer-wise traversal to populate the matrix and increments a running counter for values.

--- day10/Program.cs
+++ day10/Program.cs
@@ -38,6 +38,37 @@ namespace day10
     }
     public class Solution
     {
+        public int[][] GenerateMatrix(int n)
+        {
+            int [][] matrix = new int[n][];
+            for (int i = 0; i < n; i++)
+                matrix[i] = new int[n];
+            int num = 1;
+            int top = 0, bottom = n - 1;
+            int left = 0, right = n - 1;
+            while (top <= bottom && left <= right)
+            {
+                for (int j = left; j <= right; j++)
+                    matrix[top][j] = num++;
+                top++;
+                for (int i = top; i <= bottom; i++)
+                    matrix[i][right] = num++;
+                right--;
+                if (top <= bottom)
+                {
+                    for (int j = right; j >= left; j--)
+                        matrix[bottom][j] = num++;
+                    bottom--;
+                }
+                if (left <= right)
+                {
+                    for (int i = bottom; i >= top; i--)
+                        matrix[i][left] = num++;
+                    left++;
+                }
+            }
+            return matrix;
+        }
         public int LengthOfLastWord(string s)
         {
             int length = 0;
