1cef407ec8dcbe092c241911793a93b8551e4c56
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-04-29 00:08:13 +0300

    Replace FullJustify with MySqrt binary search
    Remove the FullJustify text-justification implementation and replace it with MySqrt(int x), an integer square-root using binary search. Handles small inputs (x < 2) and performs a midpoint square comparison, returning the floor(sqrt(x)). This simplifies day16/Program.cs to solve the integer sqrt problem instead of text justification.

--- day16/Program.cs
+++ day16/Program.cs
@@ -1,47 +1,20 @@
 ﻿public class Solution
 {
-    public IList<string> FullJustify(string[] words, int maxWidth)
+    public int MySqrt(int x)
     {
-
-        IList<string> result = new List<string>();
-        int index = 0;
-        while (index < words.Length)
+        if (x < 2) return x;
+        int left = 1, right = x / 2;
+        while (left <= right)
         {
-            int totalChars = words[index].Length;
-            int last = index + 1;
-            while (last < words.Length)
-            {
-                if (totalChars + 1 + words[last].Length > maxWidth) break;
-                totalChars += 1 + words[last].Length;
-                last++;
-            }
-            StringBuilder sb = new StringBuilder();
-            int gaps = last - index - 1;
-            if (last == words.Length || gaps == 0)
-            {
-                for (int i = index; i < last; i++)
-                {
-                    sb.Append(words[i]);
-                    if (i < last - 1) sb.Append(' ');
-                }
-                sb.Append(' ', maxWidth - sb.Length);
-            }
+            int mid = left + (right - left) / 2;
+            long square = (long)mid * mid;
+            if (square == x)
+                return mid;
+            else if (square < x)
+                left = mid + 1;
             else
-            {
-                int spaces = (maxWidth - totalChars) / gaps;
-                int extraSpaces = (maxWidth - totalChars) % gaps;
-                for (int i = index; i < last; i++)
-                {
-                    sb.Append(words[i]);
-                    if (i < last - 1)
-                    {
-                        sb.Append(' ', spaces + (i - index < extraSpaces ? 1 : 0));
-                    }
-                }
-            }
-            result.Add(sb.ToString());
-            index = last;
+                right = mid - 1;
         }
-        return result;
+        return right;
     }
 }
