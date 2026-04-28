1d7513db86385aaae7fff28401ad637eb76c7422
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-04-29 00:06:34 +0300

    Implement FullJustify text justification
    Replace placeholder Program.cs with a Solution class implementing FullJustify. Uses a greedy approach to pack words per line, evenly distributes spaces across gaps (with extra spaces assigned to left gaps), and left-justifies the last line or single-word lines. Builds lines with StringBuilder and returns IList<string> of justified lines.

--- day16/Program.cs
+++ day16/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public IList<string> FullJustify(string[] words, int maxWidth)
+    {
+
+        IList<string> result = new List<string>();
+        int index = 0;
+        while (index < words.Length)
+        {
+            int totalChars = words[index].Length;
+            int last = index + 1;
+            while (last < words.Length)
+            {
+                if (totalChars + 1 + words[last].Length > maxWidth) break;
+                totalChars += 1 + words[last].Length;
+                last++;
+            }
+            StringBuilder sb = new StringBuilder();
+            int gaps = last - index - 1;
+            if (last == words.Length || gaps == 0)
+            {
+                for (int i = index; i < last; i++)
+                {
+                    sb.Append(words[i]);
+                    if (i < last - 1) sb.Append(' ');
+                }
+                sb.Append(' ', maxWidth - sb.Length);
+            }
+            else
+            {
+                int spaces = (maxWidth - totalChars) / gaps;
+                int extraSpaces = (maxWidth - totalChars) % gaps;
+                for (int i = index; i < last; i++)
+                {
+                    sb.Append(words[i]);
+                    if (i < last - 1)
+                    {
+                        sb.Append(' ', spaces + (i - index < extraSpaces ? 1 : 0));
+                    }
+                }
+            }
+            result.Add(sb.ToString());
+            index = last;
+        }
+        return result;
+    }
+}
