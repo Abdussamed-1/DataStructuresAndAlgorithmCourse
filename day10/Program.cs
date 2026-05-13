c420ada49afb18d4e274fe7e373d7c371dc22d10
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-05-13 18:44:57 +0300

    Add ClimbStairs iterative solution
    Introduce ClimbStairs(int n) to the Solution class. Implements the classic climbing-stairs DP (Fibonacci-like) algorithm: handles base cases n <= 2 and iteratively computes the number of ways in O(n) time with O(1) space by rolling two accumulators.

--- day10/Program.cs
+++ day10/Program.cs
@@ -38,6 +38,18 @@ namespace day10
     }
     public class Solution
     {
+        public int ClimbStairs(int n)
+        {
+            if (n <= 2) return n;
+            int a = 1, b = 2;
+            for (int i = 3; i <= n; i++)
+            {
+                int temp = a + b;
+                a = b;
+                b = temp;
+            }
+            return b;
+        }
         public int MySqrt(int x)
         {
             if (x < 2) return x;
