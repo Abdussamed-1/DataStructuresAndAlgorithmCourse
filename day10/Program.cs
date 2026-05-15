38559a0828380db2bfa6404f03c0bf868957f377
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-05-15 22:16:36 +0300

    Add SortColors (Dutch national flag) method
    Implement SortColors in the Solution class to sort an int[] in-place using the three-pointer (Dutch National Flag) algorithm. Adds a private Swap helper used by the method. The implementation runs in O(n) time and O(1) extra space and mutates the input array.

--- day10/Program.cs
+++ day10/Program.cs
@@ -38,6 +38,34 @@ namespace day10
     }
     public class Solution
     {
+        public void SortColors(int[] nums)
+        {
+            int low = 0, mid = 0, high = nums.Length - 1;
+            while (mid <= high)
+            {
+                if (nums[mid] == 0)
+                {
+                    Swap(nums, low, mid);
+                    low++;
+                    mid++;
+                }
+                else if (nums[mid] == 1)
+                {
+                    mid++;
+                }
+                else
+                {
+                    Swap(nums, mid, high);
+                    high--;
+                }
+            }
+        }
+        private void Swap(int[] nums, int i, int j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
         public bool SearchMatrix(int[][] matrix, int target)
         {
             int m = matrix.Length, n = matrix[0].Length;
