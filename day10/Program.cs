914dfdd39a62d35f1b4a46918fae406641a02726
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-05-19 20:56:02 +0300

    Add GetConcatenation method to Solution
    Implements GetConcatenation(int[] nums) in the Solution class. The method returns a new array containing nums followed by nums (length 2n), copying elements into a freshly allocated array. Runs in O(n) time and uses O(n) extra space.

--- day10/Program.cs
+++ day10/Program.cs
@@ -38,6 +38,17 @@ namespace day10
     }
     public class Solution
     {
+        public int[] GetConcatenation(int[] nums)
+        {
+            int n = nums.Length;
+            int[] result = new int[2 * n];
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = nums[i];
+                result[i + n] = nums[i];
+            }
+            return result;
+        }
         public bool Exist(char[][] board, string word)
         {
             int m = board.Length, n = board[0].Length;
