7b5d05c97eff7136d03c79042437a14122856575
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-05-21 23:21:46 +0300

    Update Program.cs

--- DataStructuredType/Program.cs
+++ DataStructuredType/Program.cs
@@ -47,6 +47,24 @@ namespace Datastructuredpaths
     }
     public class arrays2 
     {
+        public int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            int n = nums.Length;
+            int[] result = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (nums[j] < nums[i])
+                    {
+                        count++;
+                    }
+                }
+                result[i] = count;
+            }
+            return result;
+        }
         public int[] FindErrorNums(int[] nums)
         {
             int n = nums.Length;
