67ea88e012136c3bac8e6346b02b0687be23494d
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-06-22 22:55:08 +0300

    Add Merge method for sorted arrays
    Implements Merge(int[] nums1, int m, int[] nums2, int n) to merge two sorted arrays into nums1 in-place using three-pointer approach from the end. The method copies larger elements to the tail and handles leftover elements from nums2. Also includes minor whitespace adjustments in Program.cs.

--- DataStructuredType/Program.cs
+++ DataStructuredType/Program.cs
@@ -436,6 +436,7 @@ namespace Datastructuredpaths
 
             return exists;
         }
+
         public ListNode Partition(ListNode head, int x)
         {
             ListNode beforeHead = new ListNode(0);
@@ -460,6 +461,7 @@ namespace Datastructuredpaths
             before.next = afterHead.next; // iki listeyi birleştir
             return beforeHead.next; // yeni baş düğümü döndür
         }
+
         public ListNode Partition(ListNode head, int x)
         {
             ListNode beforeHead = new ListNode(0);
@@ -510,5 +512,24 @@ namespace Datastructuredpaths
                 }
             }
             return false;
+        }
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int i = m - 1, j = n - 1, k = m + n - 1;
+            while (i >= 0 && j >= 0)
+            {
+                if (nums1[i] > nums2[j])
+                {
+                    nums1[k--] = nums1[i--];
+                }
+                else
+                {
+                    nums1[k--] = nums2[j--];
+                }
+            }
+            while (j >= 0)
+            {
+                nums1[k--] = nums2[j--];
+            }
         }
     }
