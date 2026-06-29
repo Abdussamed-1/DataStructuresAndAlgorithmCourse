3b1cbb49aadc3ce2bf2e73843235ce7ce3bd40f5
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-06-29 22:31:59 +0300

    Update Proogram.cs

--- day15/Program.cs
+++ day15/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
 
 class Program
 {
@@ -60,5 +61,26 @@ class Program
         }
 
         Console.ReadKey();
+    }
+
+    public ListNode ReverseBetween(ListNode head, int left, int right)
+    {
+        if (head == null || left == right)
+            return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        for (int i = 1; i < left; i++)
+            prev = prev.next;
+        ListNode current = prev.next;
+        ListNode next = null;
+        for (int i = 0; i < right - left; i++)
+        {
+            next = current.next;
+            current.next = next.next;
+            next.next = prev.next;
+            prev.next = next;
+        }
+        return dummy.next;
     }
 }
